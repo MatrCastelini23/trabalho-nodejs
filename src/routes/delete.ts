712f5c6f12584b69ffde1a654f69da257dbfc376
MatrCastelini23 <matheusjanocac@gmail.com>
2026-05-19 20:27:33 -0300

    Delete otimizado

--- src/routes/delete.ts
+++ src/routes/delete.ts
@@ -1,17 +1,26 @@
-import { response, request } from 'express';
+import { Response, Request } from 'express';
 import { AppDataSource } from '../banco/connection.js';
 import { User } from '../entity/User.js';
-import { Request, Response } from 'express-serve-static-core';
-import { ParsedQs } from 'qs'; //importando o Request e Response do express, para tipar os parâmetros da função deleteUsuario.
+import { userSchema } from '../schemas/userSchema.js';
 
-
-//req response, request, Request, Response, ParsedQs, Record<string, any> são tipos do express e do qs, que são usados para tipar os parâmetros da função deleteUsuario, para garantir que os tipos dos parâmetros sejam corretos e evitar erros de tipo durante a execução da função.
-export async function deleteUsuario(req: Request<{ id: string; }, any, any, ParsedQs, Record<string, any>>, res: Response<any, Record<string, any>, number>) {
-    const id = parseInt(req.params.id); //pegando o id do usuário a ser deletado, que é passado como parâmetro na URL, e convertendo para inteiro.
+export async function deletarUsuario(req: Request<{id: string}>, res: Response) {
+    const id = parseInt(req.params.id);
+    if (isNaN(id)) {
+        return res.status(400).json({ error: "ID inválido" });
+    }
     try {
-        await AppDataSource.getRepository(User).delete(id);
-        res.status(200).json({ message: "Usuário deletado com sucesso" });
+        const userRepository = AppDataSource.getRepository(User);
+        const usuario = await userRepository.findOne({
+            where: { id }
+        });
+        if (!usuario) {
+            return res.status(404).json({ error: "Usuário não encontrado" });
+        }
+        await userRepository.remove(usuario);
+        return res.status(200).json({ message: "Usuário deletado com sucesso" });
     } catch (error) {
-        res.status(500).json({ message: "Erro ao deletar usuário" });
+        return res.status(500).json({ message: "Erro ao deletar usuário" });
     }
 }
+
+
