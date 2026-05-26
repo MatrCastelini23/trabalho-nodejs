d260ac4629fe31c7246d0d2eae896472c6512ad7
MatrCastelini23 <matheusjanocac@gmail.com>
2026-05-26 20:49:02 -0300

    Nome das colunas padronizados para o ingles

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,27 +1,27 @@
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
-@Entity('users') // É uma boa prática definir explicitamente o nome da tabela em plural
+@Entity('User') // É uma boa prática definir explicitamente o nome da tabela em plural
 export class User {
     @PrimaryGeneratedColumn({ type: 'int' })
     id!: number; // Usado '!' para evitar erros de inicialização do TypeScript
 
     @Column({ type: "varchar", length: 100 })
-    nome!: string;
+    name!: string;
 
     @Column({ type: "varchar", length: 100, unique: true })
     email!: string; 
 
     @Column({ type: "varchar", length: 100,})
-    senha!: string;
+    password!: string;
 
     @Column({ type: "int" })
-    idade!: number;
+    age!: number;
 
     @CreateDateColumn({ type: "datetime" }) // Mude para "timestamp" se usar PostgreSQL
-    createdAt!: Date;
+    created_at!: Date;
 
     @UpdateDateColumn({ type: "datetime" })
-    updatedAt!: Date;
+    updated_at!: Date;
 }
 
 export default User;
